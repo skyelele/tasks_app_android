20741d98aefb94f48097741facedadc467f9ef20
Skye Leahy <skyeleahy@protonmail.com>
2019-07-08 12:43:23 -0700

    first commit

---
+++ app/src/main/java/com/timbuchalka/tasktimer/AppProvider.java
@@ -0,0 +1,135 @@
+package com.timbuchalka.tasktimer;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+/**
+ * Created by timbuchalka on 20/10/16.
+ *
+ * Provider for the TaskTimer app. This is the only that knows about {@link AppDatabase}
+ */
+
+public class AppProvider extends ContentProvider {
+    private static final String TAG = "AppProvider";
+
+    private AppDatabase mOpenHelper;
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    static final String CONTENT_AUTHORITY = "com.timbuchalka.tasktimer.provider";
+    public static final Uri CONTENT_AUTHORITY_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    private static final int TASKS = 100;
+    private static final int TASKS_ID = 101;
+
+    private static final int TIMINGS = 200;
+    private static final int TIMINGS_ID = 201;
+
+    /*
+      private static final int TASK_TIMINGS = 300;
+      private static final int TASK_TIMINGS_ID = 301;
+     */
+
+    private static final int TASK_DURATIONS = 400;
+    private static final int TASK_DURATIONS_ID = 401;
+
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        //  eg. content://com.timbuchalka.tasktimer.provider/Tasks
+        matcher.addURI(CONTENT_AUTHORITY, TasksContract.TABLE_NAME, TASKS);
+        // e.g. content://com.timbuchalka.tasktimer.provider/Tasks/8
+        matcher.addURI(CONTENT_AUTHORITY, TasksContract.TABLE_NAME + "/#", TASKS_ID);
+
+//        matcher.addURI(CONTENT_AUTHORITY, TimingsContract.TABLE_NAME, TIMINGS);
+//        matcher.addURI(CONTENT_AUTHORITY, TimingsContract.TABLE_NAME + "/#", TIMINGS_ID);
+//
+//        matcher.addURI(CONTENT_AUTHORITY, DurationsContract.TABLE_NAME, TASK_DURATIONS);
+//        matcher.addURI(CONTENT_AUTHORITY, DurationsContract.TABLE_NAME + "/#", TASK_DURATIONS_ID);
+
+        return matcher;
+    }
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = AppDatabase.getInstance(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        Log.d(TAG, "query: called with URI " + uri);
+        final int match = sUriMatcher.match(uri);
+        Log.d(TAG, "query: match is " + match);
+
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+
+        switch(match) {
+            case TASKS:
+                queryBuilder.setTables(TasksContract.TABLE_NAME);
+                break;
+
+            case TASKS_ID:
+                queryBuilder.setTables(TasksContract.TABLE_NAME);
+                long taskId = TasksContract.getTaskId(uri);
+                queryBuilder.appendWhere(TasksContract.Columns._ID + " = " + taskId);
+                break;
+
+//            case TIMINGS:
+//                queryBuilder.setTables(TimingsContract.TABLE_NAME);
+//                break;
+
+//            case TIMINGS_ID:
+//                queryBuilder.setTables(TimingsContract.TABLE_NAME);
+//                long timingId = TimingsContract.getTimingId(uri);
+//                queryBuilder.appendWhere(TimingsContract.Columns._ID + " = " + timingId);
+//                break;
+//
+//            case TASK_DURATIONS:
+//                queryBuilder.setTables(DurationsContract.TABLE_NAME);
+//                break;
+
+//            case TASK_DURATIONS_ID:
+//                queryBuilder.setTables(DurationsContract.TABLE_NAME);
+//                long durationId = DurationsContract.getDuration(uri);
+//                queryBuilder.appendWhere(DurationsContract.Columns._ID + " = " + durationId);
+//                break;
+
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+
+        }
+
+        SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+        return queryBuilder.query(db, projection, selection, selectionArgs, null, null, sortOrder);
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+        return 0;
+    }
+}
